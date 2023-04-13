7ae9cd68170eba30344505d34bd5fcebddaad8a7
Hasse <mail@1two3code.com>
2023-04-13 23:21:50 +0200

    init

---
+++ index.ts
@@ -0,0 +1,34 @@
+import { VercelRequest, VercelResponse } from "@vercel/node";
+import { verify } from "discord-verify/node";
+
+export default async (req: VercelRequest, res: VercelResponse) => {
+  const {
+    body
+  } = req;
+  const { type } = body;
+
+  if (type === 1) {
+    res.status(200).send({ type: 1 });
+  }
+
+  const publicKey = "";
+  const signature = req.headers["x-signature-ed25519"] as string;
+  const timestamp = req.headers["x-signature-timestamp"] as string;
+  const rawBody = JSON.stringify(req.body);
+
+  const isValid = await verify(
+    rawBody,
+    signature,
+    timestamp,
+    publicKey,
+    crypto.subtle
+  );
+  //@ts-ignore
+  // const isValid = await isValidRequest(req, publicKey);
+  // if (isValid) {
+  //   res.status(200)
+  // }
+
+  // res.setHeader("Content-Type", "application/json");
+  // res.status(200).send(billboard);
+};
