6ee14e27b7865218ab114b5079f06e547d18e081
Hasse <mail@1two3code.com>
2023-04-14 00:13:19 +0200

    verify first thing

--- api/index.ts
+++ api/index.ts
@@ -2,13 +2,15 @@ import { VercelRequest, VercelResponse } from "@vercel/node";
 import isValid from "./validate-signature";
 
 export default async (req: VercelRequest, res: VercelResponse) => {
-  if (req?.body?.type === 1) {
-    return res.status(200).send({ type: 1 });
-  }
-
   if (!await isValid(req)) {
     return res.status(401).send("Invalid signature");
   }
+  
+  const { body } = req;
+  if (body?.type === 1) {
+    return res.status(200).send({ type: 1 });
+  }
+
 
 
   res.setHeader("Content-Type", "application/json");
