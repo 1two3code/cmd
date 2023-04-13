ba0d8ed91acf599e11ea98d6efd2a501139d7653
Hasse <mail@1two3code.com>
2023-04-14 00:07:06 +0200

    early return on 'ping'

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,7 @@ import isValid from "./validate-signature";
 
 export default async (req: VercelRequest, res: VercelResponse) => {
   if (req?.body?.type === 1) {
-    res.status(200).send({ type: 1 });
+    return res.status(200).send({ type: 1 });
   }
 
   if (!await isValid(req)) {
